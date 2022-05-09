614c2ad042637eea84a450afc70fb75e7d601d5c
Samantha <sammy23051996@gmail.com>
2022-05-09 18:15:39 +0300

    Initial commit

---
+++ src/main/java/prosky/java/course6/danilkovich_bot/DanilkovichBotApplication.java
@@ -0,0 +1,13 @@
+package prosky.java.course6.danilkovich_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanilkovichBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DanilkovichBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/prosky/java/course6/danilkovich_bot/DanilkovichBotApplicationTests.java
@@ -0,0 +1,13 @@
+package prosky.java.course6.danilkovich_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanilkovichBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
